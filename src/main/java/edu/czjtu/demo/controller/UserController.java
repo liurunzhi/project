9e1d56ef14bf4fd2ef027a078686f20b9945c309
Liu Runzhi <3130813352@qq.com>
2025-03-25 17:45:19 +0800

    Changes

---
+++ src/main/java/edu/czjtu/demo/controller/UserController.java
@@ -0,0 +1,49 @@
+package edu.czjtu.demo.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import edu.czjtu.demo.entity.User;
+import edu.czjtu.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public boolean save(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @PutMapping
+    public boolean update(@RequestBody User user) {
+        return userService.updateById(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable Integer id) {
+        return userService.removeById(id);
+    }
+
+    @GetMapping("/{id}")
+    public User getById(@PathVariable Integer id) {
+        return userService.getById(id);
+    }
+
+    @GetMapping("/list")
+    public List<User> list() {
+        return userService.list();
+    }
+
+    @GetMapping("/search")
+    public List<User> search(@RequestParam String name) {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like("name", name);
+        return userService.list(queryWrapper);
+    }
+} 
